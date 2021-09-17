f15bb6359185ebdd66ed72d5a17234add92dedc1
Huayan Santiago <huayan.santiago@simco.local>
2021-09-17 15:41:25 -0300

    att

--- lib/main.dart
+++ lib/main.dart
@@ -45,18 +45,31 @@ class MyHomePage extends StatelessWidget {
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                     decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2),
+                      border: Border.all(color: Colors.purple, width: 2),
                     ),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      tr.value.toString(),
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(tr.title),
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                       Text(
                         tr.date.toString(),
+                        style: TextStyle(color: Colors.grey),
                       ),
                     ],
                   )
